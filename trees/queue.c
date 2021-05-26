47864ab2aeaba2513557e84a4c8754b3d103ab1a
zetinghuang <73331777+zetinghuang@users.noreply.github.com>
2021-05-26 20:25:08 +0800

    Update queue.c

--- trees/queue.c
+++ trees/queue.c
@@ -1,6 +1,7 @@
+/*采用队列的数据结构实现树的层序遍历，队列设置为环形，即当尾指针指向第二个字节时，再加一就会恢复为第0个字节，这样能防止数据指针不断自增导致指针指向溢出。*/
 #include "queue.h"
 struct Node {
-	int data;//��������
+	int data;//´¢´æÊý¾Ý
 	struct Node* left_child;
 	struct Node* right_child;
 };
